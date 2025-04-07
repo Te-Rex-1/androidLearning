8eb0d5054be51501f3e20870ae1ece6f58c0fbbe
Abhinav Rathore <abhinavrathore478@gmail.com>
2025-04-08 00:35:09 +0530

    view model

---
+++ app/src/main/java/com/example/learing/viewModel/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.learing.viewModel
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.ViewModelProvider
+import com.example.learing.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        var viewModel=ViewModelProvider(this).get(mainAcitvitiyViewModel::class.java)
+
+        val btn=findViewById<Button>(R.id.button2)
+        val textView=findViewById<TextView>(R.id.textView)
+        textView.text=viewModel.number.toString()
+        btn.setOnClickListener{
+          viewModel.addNumber()
+            textView.text=viewModel.number.toString()
+        }
+
+    }
+}
---
+++ app/src/main/java/com/example/learing/viewModel/mainAcitvitiyViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.learing.viewModel
+
+import androidx.lifecycle.ViewModel
+
+class mainAcitvitiyViewModel:ViewModel() {
+        var number=0
+
+    fun addNumber(){
+    number++
+
+    }
+
+}
